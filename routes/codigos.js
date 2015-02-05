b9a11951adcbcd8ae75f24b17eb6a281e9834f76
sebastianfuentes <sebas6297@gmail.com>
2015-02-05 16:34:02 -0600

    post cambiado por patch

--- routes/codigos.js
+++ routes/codigos.js
@@ -18,5 +18,20 @@ router.post('/newCode/:porcentaje', function(req, res, next) {
 	});
 
 });
+router.post('/updateCode/:id', function(req, res, next) {
+	var contenido = new codigo({
+			description: req.params.status + 'status',
+			status: false // (True) Descuento no usado
+		});
+		contenido.update(function(err) {
+	  	if (err)
+	  		console.log('No se pudo guardar el descuento \n Error: ' + err);
+	  	else
+	 		console.log('Descuento guardado');
+	 		//console.log(contenido);
+	 		res.json(contenido);
+	});
+
+});
 
 module.exports = router;
