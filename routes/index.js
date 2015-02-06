290e2dc0088ca1080485fd9cb9bda7f03b2610a0
sebastianfuentes <sebas6297@gmail.com>
2015-02-06 11:15:29 -0600

    hello world

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
 module.exports = router;
